5edabc6bb1184fef4c6f3924139e146a820af42a
Reese Kunz <reesekunz21@gmail.com>
2020-03-22 21:09:01 -0700

    answered questions. finished mvp

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,9 +1,26 @@
+
+
+# * Think about what we can store in the hash table in order to help us to solve this problem more efficiently.
+# * What if we store each weight in the input list as keys? What would be a useful thing to store as the value for each key?
+# * If we store each weight's list index as its value, we can then check to see if the hash table contains an entry for `limit - weight`. If it does, then we've found the two items whose weights sum up to the `limit`!
+
+
+# Example:
+# ```
+# input: weights = [4, 6, 10, 15, 16], length = 5, limit = 21
+# output: [3, 1]
+# since these are the indices of weights 15 and 6 whose sum equals 21
+
 #  Hint:  You may not need all of these.  Remove the unused functions.
 from hashtables import (HashTable,
                         hash_table_insert,
-                        hash_table_remove,
                         hash_table_retrieve,
-                        hash_table_resize)
+                        )
+# Your function will return an instance of an `Answer` tuple that has the following form:
+# ```
+# (zero, one)
+# ```
+# where each element represents the item weights of the two packages. _**The higher valued index should be placed in the `zeroth` index and the smaller index should be placed in the `first` index.**_ If such a pair doesn’t exist for the given inputs, your function should return `None`.
 
 
 def get_indices_of_item_weights(weights, length, limit):
@@ -12,7 +29,35 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
+    # PLANNING:
+    # Insert weight values into hashtable
+    # Loop through hashtable and retrieve values.
+    # Check to see if theres any paired values (sum of values equals limit)
+    # If any paired values, compare the values to each other. Higher valued index goes first, lower valued index goes second.
+    # If no paired values that equal limit, return None.
 
+    # insert weight values into hashtable
+    # insert is looking for (hash_table, key, value)
+    for x in range(length):
+        hash_table_insert(ht, weights[x], x)
+    # grab values from hashtable
+    # retrieve  is looking for (hash_table, key)
+    for x in range(length):
+        matching_value = hash_table_retrieve(ht, (limit-weights[x]))
+        # if pair exists (values of 2 indexs equal limit)
+        if matching_value:
+            print("matching_value", matching_value)
+            print("x", x)
+            # if matching value is higher valued index, return that one first.
+            if matching_value > x:
+                print("matching_value, x = ", [matching_value, x])
+                return [matching_value, x]
+            # if matching value is lower valued index, return that one second.
+            else:
+                print("x, matching_value = ", [x, matching_value])
+                return [x, matching_value]
+        # else:
+        #     return None
     return None
 
 
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,15 +1,41 @@
+
+
 #  Hint:  You may not need all of these.  Remove the unused functions.
 from hashtables import (HashTable,
                         hash_table_insert,
-                        hash_table_remove,
                         hash_table_retrieve,
-                        hash_table_resize)
+                        )
 
 
 class Ticket:
     def __init__(self, source, destination):
-        self.source = source
-        self.destination = destination
+        self.source = source  # source is starting airport
+        self.destination = destination  # destination is next airport
+
+# ticket for first flight has source of NONE
+# ticket for final flight has a destination of NONE (no next airport)
+
+# EXAMPLE
+# tickets = [
+#   Ticket{ source: "PIT", destination: "ORD" },
+#   Ticket{ source: "XNA", destination: "CID" },
+#   Ticket{ source: "SFO", destination: "BHM" },
+#   Ticket{ source: "FLG", destination: "XNA" },
+#   Ticket{ source: "NONE", destination: "LAX" },
+#   Ticket{ source: "LAX", destination: "SFO" },
+#   Ticket{ source: "CID", destination: "SLC" },
+#   Ticket{ source: "ORD", destination: "NONE" },
+#   Ticket{ source: "SLC", destination: "PIT" },
+#   Ticket{ source: "BHM", destination: "FLG" }
+# ]
+
+# Your function should output an array of strings with the entire route of your trip. For the above example, it should look like this:
+# ```
+# ["LAX", "SFO", "BHM", "FLG", "XNA", "CID", "SLC", "PIT", "ORD"]
+
+# Hints
+# * The crux of this problem requires us to 'link' tickets together to reconstruct the entire trip. For example, if we have a ticket `('SJC', 'BOS')` that has us flying from San Jose to Boston, then there exists another ticket where Boston is the starting location, `('BOS', 'JFK')`.
+# * We can hash each ticket such that the starting location is the key and the destination is the value. Then, when constructing the entire route, the `i`th location in the route can be found by checking the hash table for the `i-1`th location.
 
 
 def reconstruct_trip(tickets, length):
@@ -19,5 +45,19 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
+    # insert is looking for (hash_table, key = x.source , value = x.destination)
+    for x in tickets:
+        hash_table_insert(hashtable, x.source, x.destination)
 
-    pass
+    # instantiate source as NONE. Setting source as key, so first trip will start from NONE.
+    source = "NONE"
+
+    # retrieve is looking for (hash_table, key), where key is the source.
+    for x in range(0, (length-1)):
+        route[x] = hash_table_retrieve(hashtable, source)
+        print("route[x]", route[x])
+        # set source to the next index value on list.
+        source = route[x]
+        print("source = ", source)
+    # -1 cause dont return None at the end
+    return route[:-1]
